2fb9f69bc18643f0a70790db123aeb6e102f9ac8
Ankit Ranjan <me@ankit.io>
2013-08-27 14:37:53 -0700

    Changed string to "string" to fix the bug

--- secner/views.py
+++ secner/views.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home', renderer=string)
+@view_config(route_name='home', renderer='string')
 def my_view(request):
 	return {'content':'This is just a test to see if everything is working.'}
